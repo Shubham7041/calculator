946e202170daa9f20a9f119cb1327d64da2648ba
Shubham <wasnikar.shubham@gmail.com>
2021-10-08 13:10:14 +0530

    Slight adjustments to css, worked on some logic

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+const button = document.querySelectorAll("button");
+const display = document.querySelector("#display");
+const clear = document.getElementById("clear");
+const operator = document.querySelectorAll(".operator");
+const equal = document.querySelector("#equals");
+
+var displayValue = "";
+var firstargument = "";
+var Operator = "";
+var secondargument = "";
+
+operator.forEach((operator) => {
+  operator.addEventListener("click", (event) => {
+    firstargument = displayValue;
+    Operator = event.target.textContent;
+  });
+});
+
+button.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    display.textContent += event.target.textContent;
+    return (displayValue = display.textContent);
+  });
+  clear.addEventListener("click", () => {
+    display.textContent = "";
+  });
+  equal.addEventListener("click", () => {
+    secondArgument = displayValue;
+  });
+});
+
+const operate = function (operator, num1, num2) {
+  if (operator == "+") {
+    return add(num1, num2);
+  } else if (operator == "-") {
+    return subtract(num1, num2);
+  } else if (operator == "/") {
+    return divide(num1, num2);
+  } else if (operator == "*") {
+    return multiply(num1, num2);
+  }
+};
+
 const add = (num1, num2) => {
   return num1 + num2;
 };
@@ -13,16 +56,3 @@ const divide = function (num1, num2) {
 const multiply = function (num1, num2) {
   return num1 * num2;
 };
-
-const button = document.querySelectorAll("button");
-const display = document.getElementById("display");
-const clear = document.getElementById("clear");
-
-button.forEach((button) => {
-  button.addEventListener("click", (event) => {
-    display.textContent += event.target.textContent;
-  });
-  clear.addEventListener("click", () => {
-    display.textContent = " ";
-  });
-});
