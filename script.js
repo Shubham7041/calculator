57ede3976803ada8d3a67933c5a6b2bc44af782f
Shubham <wasnikar.shubham@gmail.com>
2021-10-15 12:03:14 +0530

    Rewritten the whole Logic

--- script.js
+++ script.js
@@ -1,82 +1,84 @@
-const key = document.querySelector("#buttons");
-const display = document.querySelector("#display");
+const key = document.querySelector("#numbers");
+const inputDisplay = document.querySelector("#inputDisplay");
+const resultDisplay = document.querySelector("#resultDisplay");
 const clear = document.getElementById("clear");
 const operator = document.querySelectorAll(".operator");
 const equal = document.querySelector("#equals");
 
-var displayValue = "0";
+var displayValue = "";
 var firstargument = 0;
 var Operator = "";
 var waitingforsecondargument = false;
-var secondargument = null;
-/*
-operator.forEach((operator) => {
-  operator.addEventListener("click", (event) => {
-    firstargument = displayValue;
-    Operator = event.target.textContent;
-  });
-});
+var secondargument = 0;
 
-button.forEach((button) => {
-  button.addEventListener("click", (event) => {
-    inputdisplay.textContent += event.target.textContent;
-    return (displayValue = inputdisplay.textContent);
-  });
-});
-
-clear.addEventListener("click", () => {
-  inputdisplay.textContent = "";
-  resultdisplay.textContent = "";
-});
-equal.addEventListener("click", () => {
-  secondargument = displayValue;
-});
-*/
-
-function updateDisplay() {
-  display.textContent = displayValue;
+function clearDisplay() {
+  inputDisplay.textContent = "";
+  resultDisplay.textContent = "";
+  displayValue = "";
+  firstargument = 0;
+  secondargument = 0;
+  Operator = "";
+  waitingforsecondargument = false;
 }
 
-function inputdigit(digit) {
-  if (waitingforsecondargument === true) {
-    displayValue = digit;
-    waitingforsecondargument = false;
-  } else {
-    displayValue = displayValue === "0" ? digit : displayValue + digit;
+function updateDisplay() {
+  inputDisplay.innerHTML += displayValue;
+}
+
+//function inputDigit(digit) {
+//  displayValue = displayValue === "" ? digit : displayValue + digit;
+//}
+
+function manageOperator(operator) {
+  const input = Number(displayValue);
+  if (firstargument == 0) {
+    firstargument = input;
+    waitingforsecondargument = true;
   }
+  Operator = operator;
 }
 
 key.addEventListener("click", (event) => {
   const { target } = event;
-  if (!target.matches("button")) {
-    return;
-  }
-  if (target.classList.contains("operator")) {
-    handleOperator(target.value);
+  if (target.classList.contains("number")) {
+    if (displayValue == "") {
+      displayValue += target.value;
+    } else {
+      displayValue = target.value;
+    }
     updateDisplay();
   }
-  if (target.classList.contains("clear")) {
-    console.log("clear", target.value);
-    return;
-  }
-  inputdigit(target.value);
-  updateDisplay();
 });
 
-function handleOperator(nextOperator) {
-  const inputValue = parseInt(displayValue);
-  if ((firstargument = 0 && !isNaN(inputValue))) {
-    firstargument = inputValue;
-    console.log(firstargument);
-  } else if (Operator) {
-    const result = operate(Operator, firstargument, secondargument);
-    displayValue = String(result);
-    firstargument = result;
-  }
-  waitingforsecondargument = true;
-  Operator = nextOperator;
-  console.log(Operator, firstargument, displayValue);
-}
+operator.forEach((operator) => {
+  operator.addEventListener("click", (event) => {
+    const { target } = event;
+    if (target.value == "=") {
+      if (waitingforsecondargument === true) {
+        secondargument = Number(displayValue);
+        waitingforsecondargument = false;
+      }
+      const result = operate(Operator, firstargument, secondargument);
+      resultDisplay.textContent = result;
+    }
+    if (
+      target.value == "+" ||
+      target.value == "-" ||
+      target.value == "/" ||
+      target.value == "*"
+    ) {
+      if (firstargument == 0 && Operator == "") {
+        Operator = target.value;
+        firstargument = Number(displayValue);
+        displayValue = "";
+        waitingforsecondargument = true;
+      }
+      updateDisplay();
+    }
+  });
+});
+
+clear.addEventListener("click", clearDisplay);
 
 const operate = function (operator, num1, num2) {
   if (operator == "+") {
@@ -88,7 +90,7 @@ const operate = function (operator, num1, num2) {
   } else if (operator == "*") {
     return num1 * num2;
   }
-  return secondargument;
+  return num2;
 };
 
 const add = (num1, num2) => {
